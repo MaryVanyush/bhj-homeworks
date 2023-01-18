d7f3735b89d87e9835b253869cb98a3b2008a7da
Maria Vanyushkina <m199178@mail.ru>
2023-01-18 16:33:03 +0300

    changed dom

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -2,23 +2,48 @@ const rotators = Array.from(document.querySelectorAll('.rotator__case'))
 
 let delay = 1000;
 
-function f(){
-    for (let el of rotators){
-        el.style.color = el.dataset.color
-        if(el.classList.contains('rotator__case_active')) {
-            if(!el.nextElementSibling){
-                delay = el.dataset.speed
-                el.style.color = el.dataset.color
-                el.classList.remove('rotator__case_active')
-                rotators[0].classList.add('rotator__case_active')
-                break
-            }
-        delay = el.dataset.speed
-        el.classList.remove('rotator__case_active')
-        el.nextElementSibling.classList.add('rotator__case_active')
-        break
-        }
-    }
-}
+// function f(){
+//     for (let el of rotators){
+//         if(el.classList.contains('rotator__case_active')) {
+//             if(!el.nextElementSibling){
+//                 el.classList.remove('rotator__case_active')
+//                 rotators[0].classList.add('rotator__case_active')
+//                 break
+//             }
+//         el.classList.remove('rotator__case_active')
+//         el.nextElementSibling.classList.add('rotator__case_active')
+//         break
+//         }
+//     }
+// }
     
-setInterval(f, delay)
+// setInterval(f, delay)
+
+
+
+// через рекурсию
+// Добавьте через data-атрибуты тегов опции:
+// Цвет текста
+// Скорость смены слайдов
+
+(function rotatorActive(){
+    for (let el of rotators){
+        el.style.color = el.dataset.color}
+    setTimeout(function(){
+        for (let el of rotators){
+            if(el.classList.contains('rotator__case_active')) {
+                if(!el.nextElementSibling){
+                    delay = Number(el.dataset.speed)
+                    el.classList.remove('rotator__case_active')
+                    rotators[0].classList.add('rotator__case_active')
+                    break
+                }
+            delay = Number(el.dataset.speed)
+            el.classList.remove('rotator__case_active')
+            el.nextElementSibling.classList.add('rotator__case_active')
+            break
+            }
+        }
+        rotatorActive() 
+    }, delay)
+})()
