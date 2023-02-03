8f043fe31b3e479dd3ef900fe90158b552fee695
Maria Vanyushkina <m199178@mail.ru>
2023-02-03 23:56:10 +0300

    added async

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -2,12 +2,14 @@ const progress = document.getElementById('progress')
 const btnSend = document.getElementById('send')
 
 
-btnSend.addEventListener('click', f)
+btnSend.addEventListener('submit', f)
 
 function f(){
     const file = document.getElementById('file').files[0]
     event.preventDefault()
     let xhr = new XMLHttpRequest()
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload')
+    
     xhr.onprogress = function(event) {
         console.log(`Отправлено ${event.loaded} из ${event.total} байт`)
         progress.value = (event.loaded)/(event.total)
@@ -15,14 +17,14 @@ function f(){
 
     xhr.onloadend = function() {
         if (xhr.status == 200) {
-        console.log("Успех")
-    } else {
-        console.log("Ошибка " + this.status)
-    }
-    }
-    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll')
-    let formData = new FormData()
-    formData.append("myFile", file)
-    xhr.send(formData)
+            console.log("Успех")
+        } else {
+            console.log("Ошибка " + this.status)
+        }
+        }
+
     
+    let formData = new FormData()
+    formData.append('file', file)
+    xhr.send(formData)
 }
